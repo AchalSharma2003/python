0669cc84d68379399a404ff1d4834d84e9218c98
ACHAL SHARMA <achal2003sharma@gmail.com>
2026-08-07 20:25:02 +0530

    adding pattern

---
+++ 7.nested loops/pattern2.py
@@ -0,0 +1,24 @@
+"""
+11111
+22222
+33333
+44444
+55555
+"""
+
+for a in range(1, 6):
+    for b in range(1, 6):
+        print(a, end = " ")
+    print()
+
+"""
+55555
+44444
+33333
+22222
+11111"""
+
+for i in range(5, 0, -1):
+    for j in range(5, 0, -1):
+        print(i, end = " ")
+    print()
---
+++ 7.nested loops/pattern5.py
@@ -0,0 +1,12 @@
+"""
+1
+21
+321
+4321
+54321
+"""
+n = int(input("enter the number of rows : "))
+for i in range(1,n + 1):
+    for j in range(i, 0, -1):
+        print(j, end = " ")
+    print()
