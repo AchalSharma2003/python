56054032686d4809e48281ee3460b19b42dc8fd0
ACHAL SHARMA <achal2003sharma@gmail.com>
2026-08-10 13:25:36 +0530

    creating lists folder

---
+++ 9.Lists in python/1.creating_a_List.py
@@ -0,0 +1,10 @@
+# creating a list
+# names = ["achal","anant","abhishek",11,True,22.1]
+# print(names)
+# print(type(names))
+
+marks = [2,3,5,4,1,9,8]
+marks2 = [1,2,3,4,5,6]
+# print(marks * 4)
+# list operations - -,+,/,*
+print(marks + marks2)
---
+++ 9.Lists in python/built_in_functions_for_List.py
@@ -0,0 +1,17 @@
+marks = [11,32,54,23,55,54,67,89]
+
+# to get the length
+n = len(marks)
+print(f"length of marks : {n}")
+
+# max
+maxi = max(marks)
+print(f"maximum of marks : {maxi}")
+
+# min
+mini = min(marks)
+print(f"minimum of marks : {mini}")
+
+# total
+total = sum(marks)
+print(f"total of marks : {total}")
