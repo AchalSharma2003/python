3f0ecd9c3b828a55ee5e5c01f4419ffc2b6cf615
ACHAL SHARMA <achal2003sharma@gmail.com>
2026-08-21 14:48:15 +0530

    list comprehension 1

---
+++ 9.Lists in python/14.list comprehension.py
@@ -0,0 +1,8 @@
+# make a new list from 1 to 10 -> [1,2,3,4,5.....10]
+
+# new_lst = [i for i in range(1, 11)]
+# new_lst = [i for i in range(10, -1, -1)]
+
+# 1 to 10 but all squares
+new_lst = [i * i for i in range(1, 11)]
+print(new_lst)
