1e51bf91d29942665f78063709b47c24d06dd7e0
ACHAL SHARMA <achal2003sharma@gmail.com>
2026-08-22 00:36:39 +0530

    70 and Q71

---
+++ 9.Lists in python/practice/Q70.py
@@ -0,0 +1,10 @@
+"""
+Given a 3x3 matrix, print its upper triangle. Replace all elements in 
+the lower triangle (below the main diagonal) with an asterisk (*).
+"""
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
