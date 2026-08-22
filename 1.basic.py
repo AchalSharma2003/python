7c4741816bee029909c9958ef99f8e6e8f18a2b8
ACHAL SHARMA <achal2003sharma@gmail.com>
2026-08-22 23:16:09 +0530

    basic and indexing

---
+++ 1.basic.py
@@ -0,0 +1,10 @@
+my_tuples = (1, 2, 3, "achal")
+
+print(my_tuples)
+print(type(my_tuples))
+
+# immutable
+# my_tuples[0] = 100
+print(my_tuples.count(1))
+print(my_tuples.index("achal"))
+print(my_tuples)
