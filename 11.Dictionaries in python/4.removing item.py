69db23f40851f969ac436cd7e11af6d7a5160f0b
ACHAL SHARMA <achal2003sharma@gmail.com>
2026-08-23 15:35:08 +0530

    removing item

---
+++ 11.Dictionaries in python/4.removing item.py
@@ -0,0 +1,17 @@
+student = {
+    'name': 'achal', 
+    'age': 22, 
+    'gender': 'male', 
+    'city': 'delhi', 
+    'phone': 7934875683, 
+    'state': 'MP'
+}
+
+print(student, id(student))
+
+# student.pop("name")
+# student.clear
+# del student
+# del student["age"]
+
+print(student, id(student))
