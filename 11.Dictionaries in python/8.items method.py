e9abf4f38638a7bb96c4477ab5711d95e650472c
ACHAL SHARMA <achal2003sharma@gmail.com>
2026-08-26 13:05:16 +0530

    items method

---
+++ 11.Dictionaries in python/8.items method.py
@@ -0,0 +1,19 @@
+marks = {
+    "science" : 98,
+    "maths" : 99,
+    "computer" : 100,
+    "web dev" : 100,
+    "analytics" : 89
+}
+
+print(marks.items())
+# for details in marks.items():
+#     sub = details[0]
+#     mark = details[1]
+#     print(sub,mark)
+
+# for k,v in marks.items():
+#     print(k,v)
+
+for sub,mark in marks.items():
+    print(sub,mark)
