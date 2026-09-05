ff9ed516515c2d5e74510414a902ca23d176dd17
ACHAL SHARMA <achal2003sharma@gmail.com>
2026-09-05 15:33:06 +0530

    adding elements

---
+++ 12.sets in python/3.adding_elements.py
@@ -0,0 +1,8 @@
+# use add() for single elemnents, and
+# update() for multiple elements from an iterable
+
+python_fruits = {"apple", "mango"}
+python_fruits.add("kiwi") #for single element
+python_fruits.update(["banana","pineapple","papaya"])
+
+print(python_fruits)
